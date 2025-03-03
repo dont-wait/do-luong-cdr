a9bce5b1de9d416c170e48ae2f579d93388e2d7b
duyvo2005 <duyvo1954@gmail.com>
2025-03-03 13:28:50 +0700

    add reposive for login page

--- font-end/src/pages/Login/Login.jsx
+++ font-end/src/pages/Login/Login.jsx
@@ -3,14 +3,14 @@ import LoginForm from "./Components/LoginForm/LoginForm";
 
 const Login = () => {
   return (
-    <div className='container-fluid d-flex flex-column vh-100 p-4'>
+    <div className='login-page container-fluid d-flex flex-column vh-100 p-4'>
       <div className='row flex-grow-1'>
         <div className='col-sm-12 col-md-6 d-flex justify-content-center align-items-center loginBg'>
           <span className='text-center roboto-900 w-[90%]'>
             TRƯỜNG ĐẠI HỌC CÔNG THƯƠNG THÀNH PHỐ HỒ CHÍ MINH (HUIT)
           </span>
         </div>
-        <div className='col-sm-12 col-md-6 d-flex justify-content-center align-items-center p-4'>
+        <div className='col-sm-12 col-md-6 d-flex justify-content-center align-items-center'>
           <LoginForm />
         </div>
       </div>
