01fdfe4cb75f01daa87fd09df7c226fa17e12634
duyvo2005 <duyvo1954@gmail.com>
2025-03-03 15:09:32 +0700

    add animate for login page

--- font-end/src/pages/Login/Login.jsx
+++ font-end/src/pages/Login/Login.jsx
@@ -6,7 +6,7 @@ const Login = () => {
     <div className='login-page container-fluid d-flex flex-column vh-100 p-4'>
       <div className='row flex-grow-1'>
         <div className='col-sm-12 col-md-6 d-flex justify-content-center align-items-center loginBg'>
-          <span className='text-center roboto-900 w-[90%]'>
+          <span className='login-text text-center roboto-900 w-[90%]'>
             TRƯỜNG ĐẠI HỌC CÔNG THƯƠNG THÀNH PHỐ HỒ CHÍ MINH (HUIT)
           </span>
         </div>
