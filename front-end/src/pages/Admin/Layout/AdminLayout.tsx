fa363fe106fe7dfdfabe678e01d8efcf59c2a72a
duyvo2005 <duyvo1954@gmail.com>
2025-03-22 21:57:38 +0700

    feat: remove log

--- front-end/src/pages/Admin/Layout/AdminLayout.tsx
+++ front-end/src/pages/Admin/Layout/AdminLayout.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Sidebar, Content } from "../Components";
 import "./AdminLayout.css";
 
@@ -7,9 +7,6 @@ const AdminLayout = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false);
 
   const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
-  useEffect(() => {
-    console.log("admin");
-  }, []);
 
   return (
     <div className='flex h-screen overflow-hidden'>
