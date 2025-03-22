5507348b3242cfe1070faa671e11e0acd8a72a5e
duyvo2005 <duyvo1954@gmail.com>
2025-03-22 12:53:32 +0700

    feat: create admin content

--- front-end/src/pages/Admin/Components/Admin/Admin.tsx
+++ front-end/src/pages/Admin/Components/Admin/Admin.tsx
@@ -1,5 +1,16 @@
+import { CrudFormClass } from "../../../../class/CrudFormClass";
+import CrudForm from "../../../../components/CrudForm";
+import { CrudFromField } from "../../../../types/types";
+
 const Admin = () => {
-  return <div>Admin</div>;
+  const inputFields: CrudFromField[] = [
+    new CrudFormClass("Admin Id", "text", true, true),
+    new CrudFormClass("First Name", "text", true),
+    new CrudFormClass("Last Name", "text", true),
+    new CrudFormClass("Phone", "phone", true),
+    new CrudFormClass("Email", "email", true),
+  ];
+  return <CrudForm inputFields={inputFields} url={"/admins"} />;
 };
 
 export default Admin;
