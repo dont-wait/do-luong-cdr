c3fc951c64b7b752a89e29cd9ec1fd8552ca06a6
duyvo2005 <duyvo1954@gmail.com>
2025-03-23 19:33:31 +0700

    feat: create lecturer layout

--- front-end/src/services/auth.ts
+++ front-end/src/services/auth.ts
@@ -1,11 +1,23 @@
 import { AccountData } from "../types/types";
+import { ROLES } from "../types/roles";
 
 export const loginHanle = async ({ id, password }: AccountData) => {
   try {
     await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(id, password);
+    // navigate to admin
+    /* return {
+       id: "01014017",
+       role: ROLES.Admin,
+       password: "123",
+     }; */
+
+    // navigate to CNHP
+
+    // navigate to lecturer
     return {
       id: "01014017",
-      role: 2001,
+      role: ROLES.Lecturer,
       password: "123",
     };
   } catch {
