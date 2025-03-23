e7fe1982830b54e015582a9c54541f4c4e4bbe70
KrySoSad <duythuc070205@gmail.com>
2025-03-23 13:53:04 +0700

    feat: enhance academic and subject controllers with Swagger documentation

--- back-end/src/modules/academicSubject/AcademicSubject.controller.ts
+++ back-end/src/modules/academicSubject/AcademicSubject.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AcademicSubjectService } from './AcademicSubject.service';
 
+@ApiTags('academic-subject')
 @Controller('academic-subject')
 export class AcademicSubjectController {
   constructor(private readonly academicSubjectService: AcademicSubjectService) {}
 
+  @ApiOperation({ summary: 'Get all academic subjects' })
   @Get()
   getAllAcademicSubject() {
     return this.academicSubjectService.getAllAcademicSubject();
--- back-end/src/modules/subject/dto/create-subject.dto.ts
+++ back-end/src/modules/subject/dto/create-subject.dto.ts
@@ -1,29 +1,37 @@
 import { IsNotEmpty, IsNumber, IsString, IsArray } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateSubjectDto {
+    @ApiProperty({ description: 'Subject ID', example: 'SUB001' })
     @IsString()
     @IsNotEmpty()
     id: string;
 
+    @ApiProperty({ description: 'Name of the subject', example: 'Mathematics' })
     @IsString()
     @IsNotEmpty()
     subject_name: string;
 
+    @ApiProperty({ description: 'Number of practical credits', example: 2 })
     @IsNotEmpty()
     @IsNumber()
     practical_credits: number;
 
+    @ApiProperty({ description: 'Number of theoretical credits', example: 3 })
     @IsNotEmpty()
     @IsNumber()
     theoretical_credits: number;
 
+    @ApiProperty({ description: 'Subject description', nullable: true, example: 'Introduction to basic mathematics' })
     @IsString()
     description: string | null;
 
+    @ApiProperty({ description: 'Lecturer ID', example: 'LEC001' })
     @IsString()
     @IsNotEmpty()
     lecturer_id: string;
 
+    @ApiProperty({ description: 'Academic IDs', type: [String], example: ['AC001', 'AC002'] })
     @IsArray()
     @IsString({ each: true })
     @IsNotEmpty()
