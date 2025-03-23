9b00d14bba889899d9b9f4fd45dbd0c24af71a2f
dont-wait <nguyentansang15042005@gmail.com>
2025-03-23 11:38:23 +0700

    refactor: remove unused exam-detail module

--- back-end/src/modules/examDetail/dto/update-exam_detail.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateExamDetailDto } from './create-exam_detail.dto';
-
-export class UpdateExamDetailDto extends PartialType(CreateExamDetailDto) {}
