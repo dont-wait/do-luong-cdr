b2a1c399b5bf2350cd4ef6900f01b9e5f7c1af60
KrySoSad <duythuc070205@gmail.com>
2025-04-09 20:33:35 +0700

    feat: add role-based access control to academic, admin, lecturer, and user account controllers

--- back-end/src/modules/userAccount/UserAccount.controller.ts
+++ back-end/src/modules/userAccount/UserAccount.controller.ts
@@ -1,24 +1,27 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
 import { UserAccountService } from './UserAccount.service';
+import { Roles } from 'src/common/decorator/roles.decorator';
 
 @ApiTags('accounts')
 @Controller('accounts')
 export class UserAccountController {
   constructor(private readonly userAccountService: UserAccountService) {}
 
+  @Get()
+  @Roles(2001)
   @ApiOperation({ summary: 'Get all user accounts' })
   @ApiResponse({ status: 200, description: 'Return all user accounts.' })
-  @Get()
   getAllUserAccout() {
     return this.userAccountService.getAllUserAccout();
   }
 
+  @Delete(':id')
+  @Roles(2001)
   @ApiOperation({ summary: 'Delete user account' })
   @ApiParam({ name: 'id', description: 'User account ID' })
   @ApiResponse({ status: 200, description: 'User account has been deleted.' })
   @ApiResponse({ status: 404, description: 'User account not found.' })
-  @Delete(':id')
   deleteUserAccount(@Param('id') id: string) {
     return this.userAccountService.deleteUserAccount(+id);
   }
