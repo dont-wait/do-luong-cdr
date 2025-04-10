6cae90a0395d550eb25154bf8b508948ae9655c6
dont-wait <nguyentansang15042005@gmail.com>
2025-04-10 20:01:14 +0700

    refacter: updated validate to createMethod class_id if not found and updated put method

--- back-end/src/modules/class/Class.service.ts
+++ back-end/src/modules/class/Class.service.ts
@@ -32,10 +32,24 @@ export class ClassService {
   }
 
   async updateClass(id: string, newData: UpdateClassDto) {
-    const classData = await this.prisma.class.findUnique({ where: { id } });
-    if (!classData) {
+    
+    //Search class by id to update
+    const existingClass = await this.prisma.class.findUnique({ where: { id } });
+    if (!existingClass) {
       throw new BadRequestException(`Class ID ${id} not found`);
     }
+
+    //Check if subject_id and lecturer_id wanna update but not exist in database
+    const existingSubject = await this.prisma.subject.findUnique({ where: { id: newData.subject_id } });
+    if (!existingSubject) {
+      throw new BadRequestException(`Subject ID ${newData.subject_id} not found`);
+    }
+
+    const existingLecturer = await this.prisma.lecturer.findUnique({ where: { id: newData.lecturer_id } });
+    if (!existingLecturer) {
+      throw new BadRequestException(`Lecturer ID ${newData.lecturer_id} not found`);
+    }
+
     return this.prisma.class.update({ where: { id }, data: newData });
   }
 
@@ -47,12 +61,20 @@ export class ClassService {
     return this.prisma.class.delete({ where: { id } });
   }
 
+
+  //Validate for new class
   private async validateClassData(c: CreateClassDto) {
+    
+    const existingClass = await this.prisma.class.findUnique({ where: { id: c.id } });
+    if (existingClass) {
+      throw new BadRequestException(`Class ID ${c.id} already exists`);
+    }
+    
     const subjectExist = await this.prisma.subject.findUnique({ where: { id: c.subject_id } });
     if (!subjectExist) {
       throw new BadRequestException(`Subject ID ${c.subject_id} not found`);
     }
-
+  
     const lecturerExist = await this.prisma.lecturer.findUnique({ where: { id: c.lecturer_id } });
     if (!lecturerExist) {
       throw new BadRequestException(`Lecturer ID ${c.lecturer_id} not found`);
--- back-end/src/modules/classStudent/ClassStudent.controller.ts
+++ back-end/src/modules/classStudent/ClassStudent.controller.ts
@@ -1,5 +1,5 @@
 import { BadRequestException, Body, Controller, Get, Param, Post } from "@nestjs/common";
-import { ApiOperation, ApiTags } from "@nestjs/swagger";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { ClassStudentService } from "./ClassStudent.service";
 import { CreateClassStudentDto } from "./dto/createClassStudent.dto";
 
@@ -10,16 +10,25 @@ export class ClassStudentController {
 
     @Get()
     @ApiOperation({ summary: 'Get on all class student' })
+    @ApiResponse({ status: 200, description: 'Get all class student' })
+    @ApiResponse({ status: 400, description: 'Bad request' })
+    @ApiResponse({ status: 404, description: 'Class student info not found' })
     getAllClassStudent() {
         return this.classStudentService.getAllClassStudent();
     }
     @Get('class/:classId')
+    @ApiResponse({ status: 200, description: 'Get all student by class id' })
+    @ApiResponse({ status: 400, description: 'Bad request' })
+    @ApiResponse({ status: 404, description: 'Class info not found' })
     @ApiOperation({summary: 'get many student by class id'})
     getClassStudentByClassId(@Param('classId') classId) {
         return this.classStudentService.getInfoStudentsByClassId(classId);
     }
 
     @Get('student/:studentId')
+    @ApiResponse({ status: 200, description: 'Get all class by student id' })
+    @ApiResponse({ status: 400, description: 'Bad request' })
+    @ApiResponse({ status: 404, description: 'student info not found' })
     @ApiOperation({summary: 'get many class by student id'})
     getClassIdByStudentId(
         @Param('studentId') studentId) {
@@ -29,6 +38,9 @@ export class ClassStudentController {
     }
 
     @Post()
+    @ApiResponse({ status: 200, description: 'Create class student successfully' })
+    @ApiResponse({ status: 400, description: 'Bad request' })
+    @ApiResponse({ status: 404, description: 'Class or student info not found' })
     @ApiOperation({summary: 'Create class with many student'})
     createClassWithManyStudent(@Body() data: CreateClassStudentDto | CreateClassStudentDto[]) {
         return this.classStudentService.createClassStudent(data);
