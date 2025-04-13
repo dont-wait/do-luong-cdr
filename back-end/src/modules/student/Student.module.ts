c455b28649423fea3e82213f0fd3324e2af7e956
Tyuu <quagntam09@gmail.com>
2025-04-13 07:39:53 +0700

    refactor: remove unused imports and class_id from CreateStudentDto; simplify StudentModule and StudentService

--- back-end/src/modules/student/Student.module.ts
+++ back-end/src/modules/student/Student.module.ts
@@ -2,14 +2,10 @@ import { forwardRef, Module} from '@nestjs/common';
 import { StudentService } from './Student.service';
 import { StudentController } from './Student.controller';
 import { PrismaService } from '../prisma/Prisma.service';
-import { ClassStudentService } from '../classStudent/ClassStudent.service';
-import { ClassStudentModule } from '../classStudent/ClassStudent.module';
-import { ClassModule } from '../class/Class.module';
 
 @Module({
-  imports: [forwardRef(() => ClassStudentModule), ClassModule],
   controllers: [StudentController],
-  providers: [StudentService, PrismaService, ClassStudentService],
+  providers: [StudentService, PrismaService],
   exports: [StudentService],
 })
 export class StudentModule {}
